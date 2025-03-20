adb614a59dc39a53ccf4dcaf7cd30d27164a9dc5
ASUS <faris13abiyu@gmail.com>
2025-03-20 10:50:30 +0700

    Button : Tambah

--- CRUD_Sederhana/Form1.Designer.cs
+++ CRUD_Sederhana/Form1.Designer.cs
@@ -38,12 +38,13 @@
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtTelepon = new System.Windows.Forms.TextBox();
             this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 44);
+            this.label1.Location = new System.Drawing.Point(126, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
@@ -52,7 +53,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 76);
+            this.label2.Location = new System.Drawing.Point(126, 76);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(27, 13);
             this.label2.TabIndex = 1;
@@ -61,7 +62,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 108);
+            this.label3.Location = new System.Drawing.Point(126, 108);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 2;
@@ -70,7 +71,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(36, 137);
+            this.label4.Location = new System.Drawing.Point(126, 137);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(48, 13);
             this.label4.TabIndex = 3;
@@ -79,7 +80,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(36, 169);
+            this.label5.Location = new System.Drawing.Point(126, 169);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(39, 13);
             this.label5.TabIndex = 4;
@@ -87,44 +88,55 @@
             // 
             // txtNama
             // 
-            this.txtNama.Location = new System.Drawing.Point(105, 41);
+            this.txtNama.Location = new System.Drawing.Point(197, 37);
             this.txtNama.Name = "txtNama";
-            this.txtNama.Size = new System.Drawing.Size(100, 20);
+            this.txtNama.Size = new System.Drawing.Size(356, 20);
             this.txtNama.TabIndex = 5;
             // 
             // txtNIM
             // 
-            this.txtNIM.Location = new System.Drawing.Point(105, 73);
+            this.txtNIM.Location = new System.Drawing.Point(197, 69);
             this.txtNIM.Name = "txtNIM";
-            this.txtNIM.Size = new System.Drawing.Size(100, 20);
+            this.txtNIM.Size = new System.Drawing.Size(356, 20);
             this.txtNIM.TabIndex = 6;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(105, 105);
+            this.txtEmail.Location = new System.Drawing.Point(197, 101);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.Size = new System.Drawing.Size(356, 20);
             this.txtEmail.TabIndex = 7;
             // 
             // txtTelepon
             // 
-            this.txtTelepon.Location = new System.Drawing.Point(105, 134);
+            this.txtTelepon.Location = new System.Drawing.Point(197, 130);
             this.txtTelepon.Name = "txtTelepon";
-            this.txtTelepon.Size = new System.Drawing.Size(100, 20);
+            this.txtTelepon.Size = new System.Drawing.Size(356, 20);
             this.txtTelepon.TabIndex = 8;
             // 
             // txtAlamat
             // 
-            this.txtAlamat.Location = new System.Drawing.Point(105, 166);
+            this.txtAlamat.Location = new System.Drawing.Point(197, 162);
             this.txtAlamat.Name = "txtAlamat";
-            this.txtAlamat.Size = new System.Drawing.Size(100, 20);
+            this.txtAlamat.Size = new System.Drawing.Size(356, 20);
             this.txtAlamat.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(586, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Tambah";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtAlamat);
             this.Controls.Add(this.txtTelepon);
             this.Controls.Add(this.txtEmail);
@@ -144,8 +156,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -155,6 +165,8 @@
         private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.TextBox txtTelepon;
         private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
