f311cccee455d4dbe7fb76502729fcb18e36bd2a
ASUS <faris13abiyu@gmail.com>
2025-03-20 10:50:59 +0700

    Button : Hapus

--- CRUD_Sederhana/Form1.Designer.cs
+++ CRUD_Sederhana/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.txtTelepon = new System.Windows.Forms.TextBox();
             this.txtAlamat = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -130,12 +131,22 @@
             this.button1.Text = "Tambah";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(586, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Hapus";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtAlamat);
             this.Controls.Add(this.txtTelepon);
@@ -167,6 +178,7 @@
         private System.Windows.Forms.TextBox txtAlamat;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
