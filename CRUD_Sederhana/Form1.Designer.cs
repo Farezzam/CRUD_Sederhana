35da3f60cf942271d90ca00c77d6b13ddb88b092
ASUS <faris13abiyu@gmail.com>
2025-03-20 10:43:17 +0700

    textbox Email

--- CRUD_Sederhana/Form1.Designer.cs
+++ CRUD_Sederhana/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -98,11 +99,20 @@
             this.textBox2.TabIndex = 6;
             this.textBox2.Text = "txtNIM";
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(105, 105);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "txtEmail";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label5);
@@ -127,6 +137,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
