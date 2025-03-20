b7f4932fcb1620e01e29077b898c37de4e68d98b
ASUS <faris13abiyu@gmail.com>
2025-03-20 10:42:50 +0700

    textbox NIM

--- CRUD_Sederhana/Form1.Designer.cs
+++ CRUD_Sederhana/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -89,11 +90,20 @@
             this.textBox1.TabIndex = 5;
             this.textBox1.Text = "txtNama";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "txtNIM";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -116,6 +126,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
